dc987320d0eba4cb679aca3bf05184a58128b781
John Nguyen <hoangphuoc261@gmail.com>
2018-12-06 10:32:20 +0700

    Demo commit

--- Project/src/main.c
+++ Project/src/main.c
@@ -4,7 +4,6 @@
 /*Declare Struct*/
 GPIO_InitTypeDef GPIO_InitStructure;
 
-/*declare function*/
 void init_GPIO(void);
 
 
@@ -13,7 +12,8 @@ void SysTick_Handler()
 {
 	TimingDelay_Decrement();
 }
-
+  
+  
 void init_GPIO()
 {
 	/*setting PORT Led*/
